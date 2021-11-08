ed82386fd0ad6cbb30b93e2f7bf500245780719d
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-11-08 18:43:41 +0100

    Added files for service settings data model

---
+++ src/data_models/settings.py
@@ -0,0 +1,17 @@
+"""Module for describing the services settings"""
+from pydantic import BaseSettings, Field, stricturl
+
+
+class ServiceSettings(BaseSettings):
+    """Data model for the service settings"""
+    # URL for connecting to the database backend storing the authorization data
+    database_url: stricturl(tld_required=False, allowed_schemes={"mysql+mysqldb"}) = Field(
+        default=...,
+        title='Database Connection String',
+        description='The only supported scheme for connection strings is "mysql+mysqldb". '
+                    'Therefore you currently may only connect your authorization service to an '
+                    'MariaDB or MySQL database backend',
+        alias='SQLALCHEMY_DATABASE_URL',
+        env='SQL_ALCHEMY_DATABASE_URL'
+    )
+
