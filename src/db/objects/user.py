e240999436699312f81308e76cb76b01f3c0eaf8
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-11-10 11:27:52 +0100

    Added the other table specifications and created relationships between them

--- src/db/objects/user.py
+++ src/db/objects/user.py
@@ -1,5 +1,7 @@
 """Module specifying the layout of the user table and other user related tables"""
 from sqlalchemy import Column, Boolean, ForeignKey, String, Text, Integer
+from sqlalchemy.orm import relationship
+
 from .. import TableBase
 
 
@@ -15,8 +17,11 @@ class User(TableBase):
     password = Column(Text, unique=True, nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
 
+    scopes = relationship("UserScope", back_populates="user")
+    tokens = relationship("UserToken", back_populates="user")
 
-class UserScopes(TableBase):
+
+class UserScope(TableBase):
     __tablename__ = "user_scopes"
     """Name of the MySQL/MariaDB table in the database specified in the connection string"""
 
@@ -24,8 +29,11 @@ class UserScopes(TableBase):
     scope_id = Column(Integer, ForeignKey("scopes.scope_id"))
     user_id = Column(Integer, ForeignKey("users.user_id"))
 
+    user = relationship("User", back_populates="scopes")
+    scope = relationship("scope.Scope", back_populates="users")
 
-class UserRoles(TableBase):
+
+class UserRole(TableBase):
     __tablename__ = "user_roles"
     """Name of the MySQL/MariaDB table in the database specified in the connection string"""
 
@@ -33,11 +41,17 @@ class UserRoles(TableBase):
     user_id = Column(Integer, ForeignKey("users.user_id"))
     role_id = Column(Integer, ForeignKey("roles.role_id"))
 
+    user = relationship("User", back_populates="roles")
+    role = relationship("role.Role", back_populates="users")
 
-class UserTokens(TableBase):
+
+class UserToken(TableBase):
     __tablename__ = "user_roles"
     """Name of the MySQL/MariaDB table in the database specified in the connection string"""
 
     mapping_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.user_id"))
-    token_id = Column(Integer, ForeignKey("tokens.tokenID"))
+    token_id = Column(Integer, ForeignKey("tokens.token_id"))
+
+    user = relationship("User", back_populates="tokens")
+    token = relationship("Token", back_populates="tokens")
