4f80972ded5595895207d61690e7aa747275b316
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-01-05 12:26:47 +0100

    Completely removed all files and started new writing of the code
    This was necessary due to circular imports being needed throughout the service which are not supported by python

---
+++ database/__init__.py
@@ -0,0 +1,6 @@
+"""Database module used for ORM descriptions of the used tables"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
