646d7855214dfb8efa5a2eee6024c23891345153
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-07-13 11:53:25 +0200

    Added tools for running queries to kong and storing an access token in the gateway

--- tools.py
+++ tools.py
@@ -1,6 +1,12 @@
 import asyncio
 import time
 
+import requests
+
+import configuration
+import enums
+import models.common
+
 
 async def is_host_available(host: str, port: int, timeout: int = 10) -> bool:
     """Check if the specified host is reachable on the specified port
@@ -13,9 +19,7 @@ async def is_host_available(host: str, port: int, timeout: int = 10) -> bool:
     while time.time() < _end_time:
         try:
             # Try to open a connection to the specified host and port and wait a maximum time of five seconds
-            _s_reader, _s_writer = await asyncio.wait_for(
-                asyncio.open_connection(host, port), timeout=5
-            )
+            _s_reader, _s_writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=5)
             # Close the stream writer again
             _s_writer.close()
             # Wait until the writer is closed
@@ -25,3 +29,36 @@ async def is_host_available(host: str, port: int, timeout: int = 10) -> bool:
             # Since the connection could not be opened wait 5 seconds before trying again
             await asyncio.sleep(5)
     return False
+
+
+def query_kong(path: str, method: enums.HTTPMethod, data: None | dict) -> requests.Response:
+    _kong = configuration.KongGatewayInformation()
+    match method:
+        case enums.HTTPMethod.GET:
+            return requests.get(f"http://{_kong.hostname}:{_kong.admin_port}{path}")
+        case enums.HTTPMethod.POST:
+            return requests.post(f"http://{_kong.hostname}:{_kong.admin_port}{path}", data=data)
+        case enums.HTTPMethod.PUT:
+            return requests.put(f"http://{_kong.hostname}:{_kong.admin_port}{path}", data=data)
+        case enums.HTTPMethod.PATCH:
+            return requests.patch(f"http://{_kong.hostname}:{_kong.admin_port}{path}", data=data)
+        case enums.HTTPMethod.DELETE:
+            return requests.delete(f"http://{_kong.hostname}:{_kong.admin_port}{path}", data=data)
+        case _:
+            raise Exception(
+                "The function only supports the following HTTP request types: GET, POST, PUT, PATCH, DELETE"
+            )
+
+
+def store_token_in_gateway(token_set: models.common.TokenSet, username: str):
+    credential_id = open("/.credential_id", "rt").read()
+    request_data = {
+        "credential.id": credential_id,
+        "token_type": token_set.access_token_type,
+        "access_token": token_set.access_token,
+        "refresh_token": token_set.refresh_token,
+        "expires_in": token_set.expires_in,
+        "scope": token_set.scopes,
+        "authenticated_userid": username,
+    }
+    response = query_kong("/oauth2_tokens", method=enums.HTTPMethod.POST, data=request_data)
